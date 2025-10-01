5abbb6a83c537e78740539466484b52b7ff9f8bb
Riley <rileykhernandez@gmail.com>
2025-10-01 11:39:19 -0700

    impl nau7802 for scale

---
+++ components/scale.py
@@ -0,0 +1,39 @@
+import time
+from typing import Self
+from cedargrove_nau7802 import NAU7802  # type: ignore
+from dataclasses import dataclass
+import board   # type: ignore
+
+@dataclass
+class Scale:
+    nau7802: NAU7802
+    gain: float
+    offset: float
+    sample_period_millis: int
+
+    @classmethod
+    def new(cls, gain: float, offset: float, sample_period_millis: int = 250) -> Self:
+        nau7802 = NAU7802(board.I2C(), address=0x2A, active_channels=1)
+        nau7802.reset()
+        nau7802.enable()
+        return cls(nau7802, gain, offset, sample_period_millis)
+
+    def read(self) -> float:
+        return self.nau7802.read()
+    #
+    # def live_weigh(self) -> float:
+    #     return self.read()*self.gain + self.offset
+    #
+    # def weigh(self, samples) -> float:
+    #     weights: list[float] = []
+    #     for _sample in range(samples):
+    #         weights += [self.live_weigh()]
+    #         time.sleep(self.sample_period_millis/1000)
+    #     weights.sort()
+    #     return weights[samples//2]
+
+if __name__ == "__main__":
+    scale = Scale.new(gain=10072484.2624, offset=35.065176)
+    for _ in range(10):
+        print("Weight: ", scale.read())
+        time.sleep(0.25)
